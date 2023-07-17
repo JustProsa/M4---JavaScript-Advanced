c6c9ae65e2444a7cc36e47538ef4f4677a43ca9e
JustProsa <90556911+JustProsa@users.noreply.github.com>
2023-07-17 13:51:37 +0200

    M4 - Javascript Advanced

---
+++ M4-D1/script.js
@@ -0,0 +1,9 @@
+// 1. Che cos'è un algoritmo? Prova a spiegarlo a parole tue, come se lo dovessi spiegare a qualcuno che non capisce nulla di informatica.
+// 2. Che cos'è una variabile?
+// 3. undefined e null sono la stessa cosa?
+
+let num1 = 1;
+let num2 = 2;
+let num3 = num1 + num2;
+
+console.log(num3);
