a4760f831e3d58a9e0779fc3937c07901d9878f2
JustProsa <90556911+JustProsa@users.noreply.github.com>
2023-07-19 01:04:42 +0200

    M4-D1

--- M4-D1/script.js
+++ M4-D1/script.js
@@ -113,3 +113,132 @@ function acronimo(frase) {
 }
 
 console.log(acronimo(frase));
+
+//ESERCIZI EXTRA
+
+console.log("Esercizi Extra");
+
+// 1. Partendo da una stringa (passata come parametro), ritorna il carattere più usato nella stringa stessa.
+// 2. Controlla che due stringhe passate come parametri siano gli anagrammi l’una dell’altra. Ignora punteggiatura e spazi e ricordate di rendere la stringa tutta in minuscolo. Se le due parole sono anagrammi, ritorna true , altrimenti ritorna `false`.
+
+const stringa1 = "cartine";
+const stringa2 = "incerta";
+
+function anagrammi(stringa1, stringa2) {
+  // Rimuove la punteggiatura e gli spazi, e converge le stringhe in minuscolo
+  const cleanedString1 = stringa1.replace(/[^\w]/g, "").toLowerCase();
+  const cleanedString2 = stringa2.replace(/[^\w]/g, "").toLowerCase();
+
+  if (cleanedString1.length !== cleanedString2.length) {
+    // Se le lunghezze delle due stringhe dopo la pulizia sono diverse, non possono essere anagrammi
+    return false;
+  }
+
+  // Converte le stringhe pulite in array
+  const stringa1Array = cleanedString1.split("");
+  const stringa2Array = cleanedString2.split("");
+
+  // Ordina gli array per confrontarli facilmente
+  stringa1Array.sort();
+  stringa2Array.sort();
+
+  // Confronta i due array
+  for (let i = 0; i < stringa1Array.length; i++) {
+    if (stringa1Array[i] !== stringa2Array[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(anagrammi(stringa1, stringa2)); // Stampa true
+
+// 3. Partendo da una lista di possibili anagrammi e da una parola (entrambi passati come parametri), ritorna un nuovo array contenente tutti gli anagrammi corretti della parola data.
+// Per esempio, partendo da “cartine” e [”carenti”, “incerta”, “espatrio”], il valore ritornato deve essere [”carenti”, “incerta”].
+
+const parola = "cartine";
+const paroleAnagrammi = ["carenti", "incerta", "espatrio"];
+
+function forseAnagrammi(parola, paroleAnagrammi) {
+  let sonoAnagrammi = [];
+  let cleanedString1 = parola.replace(/[^\w]/g, "").toLowerCase();
+
+  for (let el of paroleAnagrammi) {
+    let cleanedString2 = el.replace(/[^\w]/g, "").toLowerCase();
+
+    let array1 = cleanedString1.split("").sort();
+    let array2 = cleanedString2.split("").sort();
+
+    if (array1.join("") === array2.join("")) {
+      sonoAnagrammi.push(el);
+    }
+  }
+
+  return sonoAnagrammi;
+}
+
+console.log(forseAnagrammi(parola, paroleAnagrammi));
+
+// 4. Partendo da una stringa passata come parametro, ritorna `true` se la stringa è palindroma o `false` se non lo è.
+
+const palindroma = "anna";
+
+function parolaPalindroma(palindroma) {
+  let palindromaReverse = palindroma.split("").reverse().join("");
+
+  return palindroma === palindromaReverse;
+}
+
+console.log(parolaPalindroma(palindroma));
+
+// 5. Partendo da un numero intero (dai parametri) ritorna un numero che contenga le stesse cifre, ma in ordine contrario. Es. 189 ⇒ 981
+
+const number = 971;
+
+function numberReverse(num) {
+  return parseInt(num.toString().split("").reverse().join(""));
+}
+
+console.log(numberReverse(number));
+
+// 6. Scrivi una funzione che accetti un numero positivo X come parametro. La funzione dovrebbe stampare a console una “scala” creata con il carattere “#” e avente X scalini.
+
+const numeroScalini = 7;
+
+function scala(x) {
+  for (let i = 1; i <= x; i++) {
+    let asterischi = "#".repeat(i);
+    console.log(asterischi);
+  }
+}
+
+scala(numeroScalini);
+
+// 7. Crea una funzione che, data una stringa come parametro, ritorni la stessa stringa, ma al contrario. Es. “Ciao” ****⇒ “oaiC”
+
+const word = "Ciao";
+
+function parolaAlContrario(parola) {
+  wordReverse = parola.split("").reverse().join("");
+  console.log(wordReverse);
+}
+
+parolaAlContrario(word);
+
+//8. Crea una funzione che accetti un array e un numero Y come parametro. Dividi l’array in sotto-array aventi lunghezza Y.
+// Es. array: [1, 2, 3, 4], y: 2 ⇒ [[ 1, 2], [3, 4]]
+// array: [1, 2, 3, 4, 5], y: 4 ⇒ [[ 1, 2, 3, 4], [5]]
+
+const numeriAcaso = [7, 5, 9, 12, 27, 3, 13, 975, 17];
+
+function arraySlicer(array, y) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i += y) {
+    newArray.push(array.slice(i, i + y));
+  }
+
+  console.log(newArray);
+}
+
+arraySlicer(numeriAcaso, 6);
